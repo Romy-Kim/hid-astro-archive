ee18181a10c1dcd731892008c97c28bb1aa37d1a
bloom <bloom.drawingmusic@gmail.com>
2026-06-25 23:09:09 +0900

    Fix 배포 환경 site 주소 설정 및 화면 전환 수정

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  // 이 부분을 보연님의 도메인 주소로 정확히 적어주세요!
+  site: 'https://hidarchive.com', 
+});
